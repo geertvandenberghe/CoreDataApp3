725315ec46f4f8fee905b92a47a4895b8911051b
Geert Van den Berghe <geertvandenberghe@geerts-mac.local>
2018-03-02 04:34:25 -0800

    Initial Commit

---
+++ CoreDataApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataApp3
+//
+//  Created by Geert Van den Berghe on 2/03/18.
+//  Copyright © 2018 Geert Van den Berghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
